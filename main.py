638a549870dee9300e7be04dc1c0ff0eb019e9fb
WilDonLon <wjd004@uark.edu>
2024-04-13 10:36:15 -0500

    Prints a list of messages that the user will be able to choose from. Choice implimentation not started, nor finished. Also added missed goals counter "self.missed". It incriments each time the user enters a shitty number.

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.pagelayout import PageLayout
-
-import send_sms
-import random
-import pandas as pd
+from kivy.uix.relativelayout import RelativeLayout
+# from kivy.graphics import *
 
 class UserInput(BoxLayout):
     def __init__(self, **kwargs):
@@ -21,13 +19,25 @@ class UserInput(BoxLayout):
         self.add_widget(self.stepCountInput)
         self.text.bind()
         self.stepCountInput.bind(on_text_validate=self.checkStepCount)
+        self.missed = 0
 
     def checkStepCount(self, instance):
-        if int(instance.text) < 5000:
-            send_sms.send_dumb_sms()
+        hotdog = ['hi', 'lo', 'deez', 'nuts']
+        if int(instance.text) < 2000:
+            # Use a lambda function inside the formatted string to output each item in the list hotdog along with its index followed by a new line
+            self.missed += 1
+            self.text.text = f"Uh oh! That was a poor decision. You have missed {self.missed} goals. \nNow you must choose...\n" + ''.join(map(lambda x: f'Message {x[0] + 1}: {x[1]}\n', enumerate(hotdog))) + "\nChoose a message to send, you naughty little slacker."
+        else:
+            self.text.text = "Gooooood~~"
 
-        elif int(instance.text) >= 5000:
-            self.text.text = "Good...You are spared for one more day..."
+
+
+# class Punishments(RelativeLayout):
+#     def __init__(self, **kwargs):
+#         super(Punishments, self).__init__(**kwargs)
+#     with self.canvas:
+#         Color(1., 0, 0)
+#         Rectangle(pos=(10,10), size=(500,500))
 
 class FlipPages(PageLayout):
     def __init__(self, **kwargs):
